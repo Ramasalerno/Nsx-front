ab768115d8cd6043dfd30b462682702c2c063cfa
ramiro <ramiro_salerno@solutionbox.com.ar>
2025-03-25 15:15:42 -0300

    Modifico valores firebase

--- src/components/pages/Configurador/firebase.js
+++ src/components/pages/Configurador/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDR7XnBuuF1ArBMhhiOLlosBZQgjtr1dx8",
-  authDomain: "test-sbox2022.firebaseapp.com",
-  projectId: "test-sbox2022",
-  storageBucket: "test-sbox2022.appspot.com",
-  messagingSenderId: "922164950295",
-  appId: "1:922164950295:web:3a79b058d64f6a67c5b352"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
